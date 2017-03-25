502f6549ff0c16aec56a6fd310e35ff3439597c3
Henry V. Hardman <hvh237@email.vccs.edu>
2017-03-25 12:13:39 +0300

    @hhardman34 03.25.2017 12:16AM

--- src/jrdesktop/rmi/server/ServerInterface.java
+++ src/jrdesktop/rmi/server/ServerInterface.java
@@ -16,62 +16,62 @@ import java.util.Hashtable;
 
 public interface ServerInterface extends Remote {
 
-    public boolean isOptionsChanged(int index) throws RemoteException;
+    boolean isOptionsChanged(int index) throws RemoteException;
 
-    public void setOptionsChanged(int index, boolean bool) throws RemoteException;
+    void setOptionsChanged(int index, boolean bool) throws RemoteException;
 
-    public int startViewer(InetAddress inetAddress,
+    int startViewer(InetAddress inetAddress,
                            String username, String password, boolean isReversedConnection) throws RemoteException;
 
-    public void stopViewer(int index) throws RemoteException;
+    void stopViewer(int index) throws RemoteException;
 
-    public HashMap<String, byte[]> getChangedScreenBlocks(int index,
-                                                          boolean isEmpty) throws RemoteException;
+    HashMap<String, byte[]> getChangedScreenBlocks(int index,
+                                                   boolean isEmpty) throws RemoteException;
 
-    public void setChangedScreenBlocks(HashMap<String, byte[]> changedBlocks,
-                                       int index) throws RemoteException;
+    void setChangedScreenBlocks(HashMap<String, byte[]> changedBlocks,
+                                int index) throws RemoteException;
 
-    public byte[] getScreenCapture(int index) throws RemoteException;
+    byte[] getScreenCapture(int index) throws RemoteException;
 
-    public void setScreenCapture(byte[] data, int index) throws RemoteException;
+    void setScreenCapture(byte[] data, int index) throws RemoteException;
 
-    public Rectangle getScreenRect(int index) throws RemoteException;
+    Rectangle getScreenRect(int index) throws RemoteException;
 
-    public void setScreenRect(Rectangle rect, int index) throws RemoteException;
+    void setScreenRect(Rectangle rect, int index) throws RemoteException;
 
-    public ArrayList getMouseEvents(int index) throws RemoteException;
+    ArrayList getMouseEvents(int index) throws RemoteException;
 
-    public void setMouseEvents(int index, ArrayList events) throws RemoteException;
+    void setMouseEvents(int index, ArrayList events) throws RemoteException;
 
-    public ArrayList getKeyEvents(int index) throws RemoteException;
+    ArrayList getKeyEvents(int index) throws RemoteException;
 
-    public void setKeyEvents(ArrayList events) throws RemoteException;
+    void setKeyEvents(ArrayList events) throws RemoteException;
 
-    public Object getClipboardContent() throws RemoteException;
+    Object getClipboardContent() throws RemoteException;
 
-    public void setClipboardContent(Object object) throws RemoteException;
+    void setClipboardContent(Object object) throws RemoteException;
 
-    public Object getClipboardContent(int index) throws RemoteException;
+    Object getClipboardContent(int index) throws RemoteException;
 
-    public void setClipboardContent(Object object, int index) throws RemoteException;
+    void setClipboardContent(Object object, int index) throws RemoteException;
 
-    public Object getOptions(int index) throws RemoteException;
+    Object getOptions(int index) throws RemoteException;
 
-    public void setOptions(Object data, int index) throws RemoteException;
+    void setOptions(Object data, int index) throws RemoteException;
 
-    public Object getOption(int index, int option) throws RemoteException;
+    Object getOption(int index, int option) throws RemoteException;
 
-    public void setOption(Object data, int index, int option) throws RemoteException;
+    void setOption(Object data, int index, int option) throws RemoteException;
 
-    public ArrayList getFileList() throws RemoteException;
+    ArrayList getFileList() throws RemoteException;
 
-    public byte[] ReceiveFile(String fileName, int index) throws RemoteException;
+    byte[] ReceiveFile(String fileName, int index) throws RemoteException;
 
-    public void SendFile(byte[] filedata, String fileName, int index) throws RemoteException;
+    void SendFile(byte[] filedata, String fileName, int index) throws RemoteException;
 
-    public ArrayList getConnectionInfos(int index) throws RemoteException;
+    ArrayList getConnectionInfos(int index) throws RemoteException;
 
-    public Hashtable getHostProperties() throws RemoteException;
+    Hashtable getHostProperties() throws RemoteException;
 
-    public void setHostProperties(int index, Hashtable props) throws RemoteException;
+    void setHostProperties(int index, Hashtable props) throws RemoteException;
 }
